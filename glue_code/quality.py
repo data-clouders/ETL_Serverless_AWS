1e0a04d252600ced47a58a88e27ccfff6f7a61f4
diegoalsls <diegoalsls@gmail.com>
2025-01-24 10:36:40 -0500

    Se modifica quality.py después del debug

--- glue_code/quality.py
+++ glue_code/quality.py
@@ -1,17 +1,14 @@
 import sys
-from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, to_date, date_format
+from pyspark.sql.functions import col, to_date, date_format, to_timestamp, udf, StringType, dayofmonth, month, year
 from datetime import datetime
 from pyspark.sql.utils import AnalysisException
-import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from pyspark.sql.types import IntegerType, DateType, DoubleType, DecimalType
-
 
 # Parse command-line arguments
 args = getResolvedOptions(sys.argv,
@@ -33,8 +30,8 @@ job.init(args['JOB_NAME'], args)
 def convert_date_columns(df, columns_to_cast):
     
     for col_name in columns_to_cast:
-        df = df.withColumn(col_name, date_format(df[f"{col_name}"], "MM/dd/yyyy"))
-    
+      df = df.withColumn(col_name, substring(df[col_name], 1, 10))
+      df = df.withColumn(col_name, df[f"{col_name}"].cast(DateType()))
     return df
 
 
@@ -72,7 +69,12 @@ def validate_and_write_parquet(df, database_name, table_name, output_s3_path, sp
         # If table doesn't exist, create the table
         
         df.write.option('path',output_s3_path).format("parquet").saveAsTable(f"{database_name}.{table_name}")
-
+def get_part_date(df,column_date):
+    for column in column_date:
+      df = df.withColumn(f"day_{column}", dayofmonth(df[f"{column}"])) \
+        .withColumn(f"month_{column}", month(df[f"{column}"])) \
+        .withColumn(f"year_{column}", year(df[f"{column}"]))
+    return df
 
 def main():
     
@@ -87,24 +89,28 @@ def main():
     df_col_read = spark.read.parquet(df_input_col)
 
     # USA
-    colums_to_delete_usa = ["lastmodified", "states"]
-    columns_to_cast_number_usa = ["death", "deathincrease", "hospitalized", "hospitalizedcumulative", "hospitalizedcurrently", "hospitalizedincrease", "inicucumulative", "inicucurrently", "negative", "negativeincrease", "onventilatorcumulative", "onventilatorcurrently", "pending", "positive", "positiveincrease", "recovered", "totaltestresults", "totaltestresultsincrease"]
-    columns_to_cast_date_usa = ["date", "datechecked", "lastmodified"]
+    colums_to_delete_usa = ["date","states","recovered"]
+    columns_to_cast_number_usa = ["death", "deathincrease", "hospitalized", "hospitalizedcumulative", "hospitalizedcurrently", "hospitalizedincrease", "inicucumulative", "inicucurrently", "negative", "negativeincrease", "onventilatorcumulative", "onventilatorcurrently", "pending", "positive", "positiveincrease", "totaltestresults", "totaltestresultsincrease"]
+    columns_to_cast_date_usa = [ "datechecked", "lastModified"]
+    columns_to_get_part_date_usa = ["datechecked","lastModified"]
 
     # COL
     colums_to_delete_col = ["per_etn_"]
     columns_to_cast_number_col = ["edad"]
     columns_to_cast_date_col = ["fecha_de_notificaci_n", "fecha_diagnostico", "fecha_inicio_sintomas", "fecha_muerte", "fecha_recuperado", "fecha_reporte_web"]
+    columns_to_get_part_date_col = ["fecha_de_notificaci_n","fecha_diagnostico"]
 
     # USA
     df_usa = drop_columns(df_usa_read, colums_to_delete_usa)
-    df_usa = convert_numbers_columns(df_usa_read, columns_to_cast_number_usa)
-    df_usa = convert_date_columns(df_usa_read, columns_to_cast_date_usa)
+    df_usa = convert_numbers_columns(df_usa, columns_to_cast_number_usa)
+    df_usa = convert_date_columns(df_usa, columns_to_cast_date_usa)
+    df_usa = get_part_date(df_usa,columns_to_get_part_date_usa)
 
     # COL
     df_col = drop_columns(df_col_read, colums_to_delete_col)
-    df_col = convert_numbers_columns(df_col_read, columns_to_cast_number_col)
-    df_col = convert_date_columns(df_col_read, columns_to_cast_date_col)
+    df_col = convert_numbers_columns(df_col, columns_to_cast_number_col)
+    df_col = convert_date_columns(df_col, columns_to_cast_date_col)
+    df_col = get_part_date(df_col,columns_to_get_part_date_col)
 
     # Escribir data
     validate_and_write_parquet(df_usa, "database_usa", "table_usa", output_s3_usa, spark)
